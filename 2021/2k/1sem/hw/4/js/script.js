a7b2b148174ce71d8f2516edad4a365dab4ab938
robert <78978033+robertGGA@users.noreply.github.com>
2021-10-02 00:35:25 +0300

    calc

--- 2021/2k/1sem/hw/4/js/script.js
+++ 2021/2k/1sem/hw/4/js/script.js
@@ -1,38 +1,78 @@
 
 let buttons = document.querySelectorAll('button');
 
-let label = document.getElementById("result")
+let label = document.getElementById("result");
+
+let equalButton = document.getElementById("equal");
+
+let resetButton = document.getElementById("reset")
+
+
 
 buttons.forEach(function(btn) {
     btn.addEventListener('click', function (e){
-        label.innerHTML += operation(btn);
+        if(btn.textContent.trim() == 'delete') {
+            remove()
+        } else {
+            if(isNaN(parseInt(label.textContent.toString()[label.textContent.toString().length - 1])) && isNaN(parseInt((btn.textContent.toString().trim())))) {
+
+            } else {
+                label.innerText += btn.textContent.trim();
+            }
+            if (label.textContent === "0") {
+                label.innerText = "";
+            }
+        }
+
     })
 })
 
-function operation(btn) {
-    if(label.textContent.trim() === '') {
-        switch (btn.textContent.trim()) {
-            case "1":
-                return 1;
-            case "2":
-                return 2;
-            case "3":
-                return 3;
-            case "4":
-                return 4;
-            case "5":
-                return 5;
-            case "6":
-                return 6;
-            case "7":
-                return 7;
-            case "8":
-                return 8;
-            case "9":
-                return 9;
-            default:
-                alert("first is a number, second - operation")
-                return "";
+equalButton.addEventListener('click', function () {
+    equal();
+})
+
+resetButton.addEventListener('click', function () {
+    label.innerText = "";
+})
+
+document.addEventListener('keydown', function(event) {
+    console.log(event.key)
+    if (checkKey(event.key)) {
+        if (event.key == '=' || event.key == 'Enter') {
+            equal(event.key);
+        } else if(event.key == 'Backspace') {
+            remove()
+        } else {
+            if(isNaN(parseInt(label.textContent.toString()[label.textContent.toString().length - 1]))
+                && isNaN(parseInt((event.key.toString().trim())))) {
+
+            } else {
+                label.innerText += event.key;
+            }
+
+            if (label.textContent === "0") {
+                label.innerText = "";
+            }
         }
     }
+});
+
+function checkKey(key) {
+    return (key >= '0' && key <= '9') || key == '+' || key == '-' || key == '/' || key == '%' ||
+        key == '*' || key == '=' || key == 'Backspace' || key == 'Enter';
+}
+
+function equal(key) {
+    if(label.textContent != "" && key == 'Enter') {
+        label.innerText = eval(label.textContent.trim());
+    } else if (label.textContent != "") {
+        if(label.textContent[label.textContent.toString().length - 1] == '=') {
+            remove()
+        }
+        label.innerText = eval(label.textContent.trim());
+    }
+}
+
+function remove() {
+    label.innerText = label.textContent.substring(0, label.textContent.length - 1)
 }
